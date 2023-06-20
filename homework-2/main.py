53855232f04355e6a01c4a7f8276c69e1bd380b3
Danila <icy.calmness@gmail.com>
2023-06-20 21:16:09 +0300

    add correction code in main.py

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,12 +1,15 @@
 from src.channel import Channel
+from src.config import CHANNEL_ID
 
 if __name__ == '__main__':
-    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    moscowpython = Channel(CHANNEL_ID)
 
     # получаем значения атрибутов
-    print(moscowpython.title)  # MoscowPython
+    print(moscowpython.channel_title)  # MoscowPython
     print(moscowpython.video_count)  # 685 (может уже больше)
-    print(moscowpython.url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
+    print(moscowpython.channel_url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
+
+    # moscowpython.print_info()
 
     # менять не можем
     moscowpython.channel_id = 'Новое название'
