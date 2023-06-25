62e246c413be52919a66e67d0cb869838728f0e5
Danila <icy.calmness@gmail.com>
2023-06-25 13:29:38 +0300

    add import CHANNEL_ID, CHANNEL_ID_TWO in homework_3.main.py

--- homework-3/main.py
+++ homework-3/main.py
@@ -1,9 +1,10 @@
 from src.channel import Channel
+from src.config import CHANNEL_ID, CHANNEL_ID_TWO
 
 if __name__ == '__main__':
     # Создаем два экземпляра класса
-    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-    highload = Channel('UCwHL6WHUarjGfUM_586me8w')
+    moscowpython = Channel(CHANNEL_ID)
+    highload = Channel(CHANNEL_ID_TWO)
 
     # Используем различные магические методы
     print(moscowpython)  # 'MoscowPython (https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A)'
